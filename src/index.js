1b89dcf0f3056794ce9d8ab743d361d8b9d2eb31
Chooli Yip <chooli.yip@gmail.com>
2018-10-17 15:12:12 -0400

    Project initiate

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import './index.css'
+
+const Index = () => {
+    return <h4>This is the start of POC React App</h4>;
+};
+
+ReactDOM.render(<Index />, document.getElementById("root"));
